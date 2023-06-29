b1a63f05b62e2cc95dd16a60a3fa01c4540d2ff7
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-06-28 23:46:38 -0400

    chore: extract some components

--- lib/elixir_tools/pages/next_ls_page.ex
+++ lib/elixir_tools/pages/next_ls_page.ex
@@ -9,195 +9,199 @@ defmodule ElixirTools.NextLSPage do
 
   def template(_assigns) do
     temple do
-      h2 class: "font-medium text-2xl text-gray-800 py-2 mb-4" do
-        "NextLS"
-      end
-
-      p do
-        "NextLS is the language server for Elixir that"
-        span class: "italic", do: "just works."
-      end
-
-      p do
-        "Still in heavy development, but early adopters are encouraged!"
-      end
-
-      p class: "inline-flex flex-col" do
-        span do
-          "GitHub:"
-
-          a href: "https://github.com/elixir-tools/next-ls",
-            target: "_blank",
-            do: "https://github.com/elixir-tools/next-ls"
+      section class: "max-w-5xl" do
+        h2 class: "font-medium text-2xl text-gray-800 py-2 mb-4" do
+          "NextLS"
         end
 
-        span do
-          "Hex:"
+        c &markdown/1,
+          text: """
+          NextLS is the language server for Elixir that _just works._
 
-          a href: "https://hex.pm/packages/next_ls",
-            target: "_blank",
-            do: "https://hex.pm/packages/next_ls"
-        end
-      end
+          Still in heavy development, but early adopters are encouraged!
+          """
 
-      p class: "italic" do
-        "The following screenshots and videos are using"
-        a href: "https://neovim.io", do: "Neovim"
-        "the"
-        a href: "https://github.com/elixir-tools/elixir-tools.nvim", do: "elixir-tools.nvim"
-        "plugin, and the"
-        a href: "https://github.com/rebelot/kanagawa.nvim", do: "Kanagawa (dragon)"
-        "colorscheme."
-      end
+        p class: "inline-flex flex-col" do
+          span do
+            "GitHub:"
 
-      h3 class: "font-medium text-xl text-gray-800 py-2" do
-        "Features"
-      end
+            a href: "https://github.com/elixir-tools/next-ls",
+              target: "_blank",
+              do: "https://github.com/elixir-tools/next-ls"
+          end
 
-      ul class: "list-disc ml-4 mb-4" do
-        li do
-          a href: "#compiler-diagnostics", class: "text-blue-500 hover:underline" do
-            "Compiler Diagnostics"
+          span do
+            "Hex:"
+
+            a href: "https://hex.pm/packages/next_ls",
+              target: "_blank",
+              do: "https://hex.pm/packages/next_ls"
           end
         end
 
-        li do
-          a href: "#code-formatting", class: "text-blue-500 hover:underline" do
-            "Code Formatting"
+        c &markdown/1,
+          text: """
+          _The following screenshots and videos are suing [Neovim](https://neovim.io), the [elixir-tools.nvim](https://github.com/elixir-tools/elixir-tools.nvim) plugin, and the [Kanagawa (dragon)](https://github.com/rebelot/kanagawa.nvim) colorscheme._
+          """
+
+        h3 class: "font-medium text-xl text-gray-800 py-2" do
+          "Features"
+        end
+
+        ul class: "list-disc ml-4 mb-4" do
+          li do
+            a href: "#compiler-diagnostics", class: "text-blue-500 hover:underline" do
+              "Compiler Diagnostics"
+            end
+          end
+
+          li do
+            a href: "#code-formatting", class: "text-blue-500 hover:underline" do
+              "Code Formatting"
+            end
+          end
+
+          li do
+            a href: "#workspace-symbols", class: "text-blue-500 hover:underline" do
+              "Workspace Symbols"
+            end
+          end
+
+          li do
+            a href: "#document-symbols", class: "text-blue-500 hover:underline" do
+              "Document Symbols"
+            end
           end
         end
 
-        li do
-          a href: "#workspace-symbols", class: "text-blue-500 hover:underline" do
-            "Workspace Symbols"
-          end
+        c &feature_header/1,
+          title: "Compiler Diagnostics",
+          id: "compiler-diagnostics",
+          version: "0.1"
+
+        c &markdown/1,
+          text: """
+          NextLS provides extremely snappy compiler diagnostics for warnings and errors emitted by the Elixir compiler.
+
+          It compiles and loads your code in it's own Erlang node, so it's completely isolated from NextLS's code.
+          """
+
+        p class: "max-w-2xl" do
+          video src:
+                  "https://res.cloudinary.com/mhanberg/video/upload/v1687729279/elixir-tools.dev/next-ls-compiler-diagnostics.mov",
+                controls: true
         end
 
-        li do
-          a href: "#document-symbols", class: "text-blue-500 hover:underline" do
-            "Document Symbols"
-          end
+        c &feature_header/1, title: "Code Formatting", id: "code-formatting", version: "0.1"
+
+        c &markdown/1,
+          text: """
+          NextLS provides extremely fast code formatting.
+
+          It formats your code in it's own Erlang node, so it always respects your formatter configuration and formatter plugins.
+
+          You'll never be left with ugly parentheses again!
+          """
+
+        p class: "max-w-2xl" do
+          video src:
+                  "https://res.cloudinary.com/mhanberg/video/upload/v1687729108/elixir-tools.dev/next-ls-code-formatting.mov",
+                controls: true
+        end
+
+        c &feature_header/1,
+          title: "Workspace Symbols",
+          id: "workspace-symbols",
+          version: "0.3"
+
+        c &markdown/1,
+          text: """
+          NextLS can provide symbol lookup for your entire codebase.
+
+          With the click of a few buttons, you can search through all of your modules, functions, macros, and structs!
+          """
+
+        p class: "max-w-2xl" do
+          video src:
+                  "https://res.cloudinary.com/mhanberg/video/upload/v1687728999/elixir-tools.dev/next-ls-workspace-symbols.mp4",
+                controls: true
+        end
+
+        c &feature_header/1, title: "Document Symbols", id: "document-symbols", version: "0.4"
+
+        c &markdown/1,
+          text: """
+          Document Symbols are similar to Workspace Symbols, but provide the symbols _only_ for the currently open document.
+
+          They also have an intrinsic hierarchy that makes the useful for seeing the relationship between the documents modules, structs, and functions.
+          """
+
+        h5 id: "document-symbols-breadcrumbs", class: "font-medium text-gray-800 py-2 mb-4" do
+          "Breadcrumbs"
+        end
+
+        c &markdown/1,
+          text: """
+          Breadcrumbs are the little "trail" you see at the top of the buffer
+          """
+
+        p class: "max-w-2xl" do
+          img src:
+                "https://res.cloudinary.com/mhanberg/image/upload/v1688007824/elixir-tools.dev/doc-symbols.png",
+              alt: "Demonstration of symbol breadcrumbs of Elixir code, powered by NextLS"
+        end
+
+        h5 id: "document-symbols-outline", class: "font-medium text-gray-800 py-2 mb-4" do
+          "Outline"
+        end
+
+        c &markdown/1,
+          text: """
+          The outline shows you the hierarchy of the entire document, and allows you to jump to a specific symbol.
+          """
+
+        p class: "max-w-2xl" do
+          img src:
+                "https://res.cloudinary.com/mhanberg/image/upload/v1688007932/elixir-tools.dev/doc-symbols-outilne.png",
+              alt: "Demonstration of symbol outline of Elixir code, powered by NextLS"
+        end
+
+        h5 id: "document-symbols-fuzzy-finder", class: "font-medium text-gray-800 py-2 mb-4" do
+          "Fuzzy Finding"
+        end
+
+        c &markdown/1,
+          text: """
+          Many editors allow you to also "fuzzy find" the document symbols, similarly to how you would search for a file to open.
+
+          The screenshot demonstrates this using the [fzf-lua](https://github.com/ibhagwan/fzf-lua) Neovim plugin.
+          """
+
+        p class: "max-w-2xl" do
+          img src:
+                "https://res.cloudinary.com/mhanberg/image/upload/v1688008012/elixir-tools.dev/doc-symbols-fzf.png",
+              alt: "Demonstration of symbol fzf of Elixir code, powered by NextLS"
         end
       end
+    end
+  end
 
-      h4 id: "compiler-diagnostics", class: "font-medium text-lg text-gray-800 py-2 mb-4" do
-        "Compiler Diagnostics"
-      end
+  def markdown(assigns) do
+    temple do
+      Earmark.as_html!(@text)
+    end
+  end
 
-      p do
-        "NextLS provides extremely snappy compiler diagnostics for warnings and errors emitted by the Elixir compiler."
-      end
+  defp feature_header(assigns) do
+    temple do
+      div class: "flex justify-between items-center py-2 mb-4" do
+        h4 id: @id, class: "font-medium text-lg text-gray-800" do
+          @title
+        end
 
-      p do
-        "It compiles and loads your code in it's own Erlang node, so it's completely isolated from NextLS's code."
-      end
-
-      p class: "max-w-2xl" do
-        video src:
-                "https://res.cloudinary.com/mhanberg/video/upload/v1687729279/elixir-tools.dev/next-ls-compiler-diagnostics.mov",
-              controls: true
-      end
-
-      h4 id: "code-formatting", class: "font-medium text-lg text-gray-800 py-2 mb-4" do
-        "Code Formatting"
-      end
-
-      p do
-        "NextLS provides extremely fast code formatting."
-      end
-
-      p do
-        "It formats your code in it's own Erlang node, so it always respects your formatter configuration and formatter plugins."
-      end
-
-      p do
-        "You'll never be left with ugly parentheses again!"
-      end
-
-      p class: "max-w-2xl" do
-        video src:
-                "https://res.cloudinary.com/mhanberg/video/upload/v1687729108/elixir-tools.dev/next-ls-code-formatting.mov",
-              controls: true
-      end
-
-      h4 id: "workspace-symbols", class: "font-medium text-lg text-gray-800 py-2 mb-4" do
-        "Workspace Symbols"
-      end
-
-      p do
-        "NextLS can provide symbol lookup for your entire codebase."
-      end
-
-      p do
-        "With the click of a few buttons, you can search through all of your modules, functions, macros, and structs!"
-      end
-
-      p class: "max-w-2xl" do
-        video src:
-                "https://res.cloudinary.com/mhanberg/video/upload/v1687728999/elixir-tools.dev/next-ls-workspace-symbols.mp4",
-              controls: true
-      end
-
-      h4 id: "document-symbols", class: "font-medium text-lg text-gray-800 py-2 mb-4" do
-        "Document Symbols"
-      end
-
-      p do
-        "Document Symbols are similar to Workspace Symbols, but provide the symbols"
-        span class: "italic", do: "only"
-        "for the currently open document."
-      end
-
-      p do
-        ~s|They also have an intrinsic hierarchy that makes the useful for seeing the relationship between the documents modules, structs, and functions.|
-      end
-
-      h5 id: "document-symbols-breadcrumbs", class: "font-medium text-gray-800 py-2 mb-4" do
-        "Breadcrumbs"
-      end
-
-      p do
-        ~s|Breadcrumbs are the little "trail" you see at the top of the buffer|
-      end
-
-      p class: "max-w-2xl" do
-        img src:
-              "https://res.cloudinary.com/mhanberg/image/upload/v1688007824/elixir-tools.dev/doc-symbols.png",
-            alt: "Demonstration of symbol breadcrumbs of Elixir code, powered by NextLS"
-      end
-
-      h5 id: "document-symbols-outline", class: "font-medium text-gray-800 py-2 mb-4" do
-        "Outline"
-      end
-
-      p do
-        ~s|The outline shows you the hierarchy of the entire document, and allows you to jump to a specific symbol.|
-      end
-
-      p class: "max-w-2xl" do
-        img src:
-              "https://res.cloudinary.com/mhanberg/image/upload/v1688007932/elixir-tools.dev/doc-symbols-outilne.png",
-            alt: "Demonstration of symbol outline of Elixir code, powered by NextLS"
-      end
-
-      h5 id: "document-symbols-fuzzy-finder", class: "font-medium text-gray-800 py-2 mb-4" do
-        "Fuzzy Finding"
-      end
-
-      p do
-        ~s|Many editors allow you to also "fuzzy find" the document symbols, similarly to how you would search for a file to open.|
-      end
-
-      p do
-        ~s|The screenshot demonstrates this using the|
-        a href: "https://github.com/ibhagwan/fzf-lua", target: "_blank", do: "fzf-lua"
-        ~s|Neovim plugin.|
-      end
-
-      p class: "max-w-2xl" do
-        img src:
-              "https://res.cloudinary.com/mhanberg/image/upload/v1688008012/elixir-tools.dev/doc-symbols-fzf.png",
-            alt: "Demonstration of symbol fzf of Elixir code, powered by NextLS"
+        span class: "italic" do
+          "Introduced in v#{@version}"
+        end
       end
     end
   end
