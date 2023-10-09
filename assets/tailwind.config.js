e089c0905b1e72b2c1af504edb82885034eb74ce
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-10-09 18:05:01 -0500

    feat: add inter for main font

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,11 +2,16 @@
 // https://tailwindcss.com/docs/configuration
 
 let plugin = require("tailwindcss/plugin");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ["./js/**/*.js", "./lib/**/*.ex"],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['"Inter"', ...defaultTheme.fontFamily.sans],
+        'fancy': ['"Rubik"', ...defaultTheme.fontFamily.sans],
+      },
       typography: {
         DEFAULT: {
           css: {
